03593d53a635810241d5567e28c4bca78d0de0e9
Snape 479 <Severus_Snape93@protonmail.com>
2021-07-14 12:17:56 -0700

    project structure

---
+++ error/src/lib.rs
@@ -0,0 +1,84 @@
+// Copyright 2021 The BMW Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use failure::{Backtrace, Context, Fail};
+use std::fmt::{self, Display};
+
+#[derive(Debug, Fail)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Fail)]
+/// ErrorKinds for BMW Node
+pub enum ErrorKind {
+    /// Config Error
+    #[fail(display = "Config Error: {}", _0)]
+    ConfigError(String),
+    /// P2P Error
+    #[fail(display = "P2P Error: {}", _0)]
+    P2P(String),
+    /// IO Error
+    #[fail(display = "IOError: {}", _0)]
+    IOError(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let cause = match self.cause() {
+            Some(c) => format!("{}", c),
+            None => String::from("Unknown"),
+        };
+        let backtrace = match self.backtrace() {
+            Some(b) => format!("{}", b),
+            None => String::from("Unknown"),
+        };
+        let output = format!(
+            "{} \n Cause: {} \n Backtrace: {}",
+            self.inner, cause, backtrace
+        );
+        Display::fmt(&output, f)
+    }
+}
+
+impl Error {
+    /// get kind
+    pub fn kind(&self) -> ErrorKind {
+        self.inner.get_context().clone()
+    }
+    /// get cause
+    pub fn cause(&self) -> Option<&dyn Fail> {
+        self.inner.cause()
+    }
+    /// get backtrace
+    pub fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Error {
+        Error {
+            inner: Context::new(ErrorKind::IOError(format!("{}", e))),
+        }
+    }
+}
---
+++ src/bin/rust-tor.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let exit_code = real_main();
+    std::process::exit(exit_code);
+}
+
+fn real_main() -> i32 {
+    println!("tor");
+    0
+}
